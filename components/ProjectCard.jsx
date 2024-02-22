aaeb77e81d81dfad9c72eed4374d43616f77ed2a
deepanshu2711 <deepanshusaini2711@gmail.com>
2024-02-22 20:49:19 +0530

    added discord clone

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -2,24 +2,24 @@ import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
 import { IoCodeSlash } from "react-icons/io5";
 
-const ProjectCard = ({imgUrl,title,description,githubLink}) => {
-    return ( 
+const ProjectCard = ({ imgUrl, title, description, githubLink }) => {
+    return (
         <div className="hover:bg-[#181818] p-3 rounded-xl hover:transition hover:scale-105 transition-all">
-        <Link href={githubLink}>
-        <div className="">
-        <div className="h-52 md:h-60 rounded-t-xl relative hover:opacity-50 group  " style={{backgroundImage: `url(${imgUrl})` ,backgroundSize:"contain" ,backgroundRepeat:"no-repeat"}} >
-            <div className=" flex items-center justify-center h-full gap-20 ">
-            </div>
+            <Link href={githubLink}>
+                <div className="">
+                    <div className="h-52 md:h-60 rounded-t-xl relative hover:opacity-50 group" style={{ backgroundImage: `url(${imgUrl})`, backgroundSize: "cover", backgroundRepeat: "no-repeat" }} >
+                        <div className=" flex items-center justify-center h-full gap-20 ">
+                        </div>
+                    </div>
+                </div>
+
+                <div className="">
+                    <h3 className="text-xl text-white mt-4">{title}</h3>
+                    <p className="text-[#ADB7BE]">{description}</p>
+                </div>
+            </Link>
         </div>
-        </div>
-            
-            <div className="">
-                <h3 className="text-xl text-white mt-4">{title}</h3>
-                <p className="text-[#ADB7BE]">{description}</p>
-            </div>
-        </Link>
-        </div>
-     );
+    );
 }
- 
+
 export default ProjectCard;
--- components/ProjectSection.jsx
+++ components/ProjectSection.jsx
@@ -1,57 +1,63 @@
 import ProjectCard from "./ProjectCard";
 
 const ProjectSection = () => {
-    return ( 
+    return (
         <div id="projects" className="flex flex-col gap-20 mt-16 justify-start items-center">
             <h2 className="text-center text-white text-4xl font-bold">My Projects</h2>
             <div className="grid grid-cols-1  md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-x-10 gap-y-10">
-                <ProjectCard 
-                imgUrl={"/realEstate.png"}
-                title={"Real Estate Web App"}
-                description={"a comprehensive real estate platform leveraging the power of MERN stack (MongoDB, Express.js, React, Node.js) alongside Firebase Authentication, Tailwind CSS for sleek styling, and JWT for secure authentication and authorization"}
-                githubLink={"https://github.com/deepanshu2711/Real-EState-MERN-stack"}
-                 />
+
 
                 <ProjectCard
-                imgUrl={"/Genius.png"}
-                title={"Genius AI Platform"}
-                description={"AI platform using next.js and clerk for authentication and authorization, Have three different AI models from Conversation , Code Generation and Image Generation "} 
-                githubLink={"https://github.com/deepanshu2711/Ai_saas"}
+                    imgUrl={"/Discord.png"}
+                    title={"Discord Clone"}
+                    description={"A fully functional discord-clone using next.js,prisma,supabase,uploadThings,liveKit( for vedio and audio calling) "}
+                    githubLink={"https://github.com/deepanshu2711/discord-clone"}
+                />
+
+
+                <ProjectCard
+                    imgUrl={"/Genius.png"}
+                    title={"Genius AI Platform"}
+                    description={"AI platform using next.js and clerk for authentication and authorization, Have three different AI models from Conversation , Code Generation and Image Generation "}
+                    githubLink={"https://github.com/deepanshu2711/Ai_saas"}
                 />
 
                 <ProjectCard
-                imgUrl={"/xlogo.png"}
-                title={"Twitter(X) Clone"}
-                description={"A Twitter clone using Next.js , MongoDb as database and clerk for authentication and authorization in which user can tweet like other tweets comment on tweets and delete tweets it also have a user profile section where user can edit their profile"} 
-                githubLink={"https://github.com/deepanshu2711/Twitter_Clone"}
+                    imgUrl={"/xlogo.png"}
+                    title={"Twitter(X) Clone"}
+                    description={"A Twitter clone using Next.js , MongoDb as database and clerk for authentication and authorization in which user can tweet like other tweets comment on tweets and delete tweets it also have a user profile section where user can edit their profile"}
+                    githubLink={"https://github.com/deepanshu2711/Twitter_Clone"}
                 />
 
-                
+
 
                 <ProjectCard
-                imgUrl={"/Blog.png"}
-                title={"Mern Blog"}
-                description={"Blog web application based on MERN (MongoDB,Express.js,React.js,Node.js) stack with Authentication jwt-cookies and redux for state management "} 
-                githubLink={"https://github.com/deepanshu2711/Mern-Blog-Beginner-Friendly"}
-                />
-
-                <ProjectCard
-                imgUrl={"/Gleam.png"}
-                title={"Gleam Google Clone"}
-                description={"A Google clone which based on next.js which uses google developers tool search engine to fetch data and return text and Images based on user search "} 
-                githubLink={"https://github.com/deepanshu2711/gleam-google-clone"}
+                    imgUrl={"/Blog.png"}
+                    title={"Mern Blog"}
+                    description={"Blog web application based on MERN (MongoDB,Express.js,React.js,Node.js) stack with Authentication jwt-cookies and redux for state management "}
+                    githubLink={"https://github.com/deepanshu2711/Mern-Blog-Beginner-Friendly"}
                 />
 
                 <ProjectCard
-                imgUrl={"/Imdb.png"}
-                title={"ImDb Clone"}
-                description={"An ImDb clone app using next.js and tailwind css for styling which uses Imdb api to fetch data"} 
-                githubLink={"https://github.com/deepanshu2711/imdb-next"}
+                    imgUrl={"/realEstate.png"}
+                    title={"Real Estate Web App"}
+                    description={"a comprehensive real estate platform leveraging the power of MERN stack (MongoDB, Express.js, React, Node.js) alongside Firebase Authentication, Tailwind CSS for sleek styling, and JWT for secure authentication and authorization"}
+                    githubLink={"https://github.com/deepanshu2711/Real-EState-MERN-stack"}
                 />
-                
+
+                <ProjectCard
+                    imgUrl={"/Gleam.png"}
+                    title={"Gleam Google Clone"}
+                    description={"A Google clone which based on next.js which uses google developers tool search engine to fetch data and return text and Images based on user search "}
+                    githubLink={"https://github.com/deepanshu2711/gleam-google-clone"}
+                />
+
+
+
+
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default ProjectSection;
