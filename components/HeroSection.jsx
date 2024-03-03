9fd52e3714d3ace71a6e98355c075de13b83f3e7
Deepanshu saini <deepanshusaini2711@gmail.com>
2024-03-03 19:20:17 +0530

    Added Download Resume Functionality

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -3,57 +3,69 @@ import Image from "next/image";
 import { TypeAnimation } from 'react-type-animation';
 
 
-    
+
 
 const HeaderSeaction = () => {
     const scrollToContact = () => {
         const contactSection = document.getElementById('contact'); // Id of the about section
         contactSection.scrollIntoView({ behavior: 'smooth' });
-      };
+    };
+
+    const handleResumeDownload = () => {
+        const fileurl = "./resume.pdf";
+        const link = document.createElement("a");
+        link.href = fileurl;
+        link.download = "resume.pdf";
+        link.click();
+    }
+
     return (
-        <section className="mt-24"> 
-        <div className="grid grid-cols-1 sm:grid-cols-12">
-            <div className="col-span-7 place-self-center text-center sm:text-left">
-            <h1 className="text-white text-4xl sm:text-5xl lg:text-6xl mb-4 font-extrabold">
-            <div className=" mb-4 text-transparent bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-clip-text">Hello, I am</div> 
-            <TypeAnimation
-            sequence={[
-                // Same substring at the start will only be typed out once, initially
-                'Deepanshu',
-                2000, // wait 1s before replacing "Mice" with "Hamsters"
-                'Web Developer',
-                2000,
-            ]}
-            wrapper="span"
-            speed={30}
-            style={{ fontSize: '1em', display: 'inline-block' }}
-            repeat={Infinity}
-            />
-            </h1>
-            <p className=" text-base sm:text-lg lg:text-xl  text-gray-400 mb-4">
-            I am ready to contribute to innovative projects and make a meaningful impact.
-            </p>
-            <div className=" flex flex-col sm:flex-row gap-3 mt-3">
-                <button  onClick={scrollToContact} className="p-3 w-full sm:w-fit text-white bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 rounded-full mr-4 hover:bg-opacity-90 font-bold border border-white">Hire Me</button>
-                <button className="p-3 w-full font-bold sm:w-fit text-white bg-transparent rounded-full border border-white 
+        <section className="mt-24">
+            <div className="grid grid-cols-1 sm:grid-cols-12">
+                <div className="col-span-7 place-self-center text-center sm:text-left">
+                    <h1 className="text-white text-4xl sm:text-5xl lg:text-6xl mb-4 font-extrabold">
+                        <div className=" mb-4 text-transparent bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-clip-text">Hello, I am</div>
+                        <TypeAnimation
+                            sequence={[
+                                // Same substring at the start will only be typed out once, initially
+                                'Deepanshu',
+                                2000, // wait 1s before replacing "Mice" with "Hamsters"
+                                'Web Developer',
+                                2000,
+                            ]}
+                            wrapper="span"
+                            speed={30}
+                            style={{ fontSize: '1em', display: 'inline-block' }}
+                            repeat={Infinity}
+                        />
+                    </h1>
+                    <p className=" text-base sm:text-lg lg:text-xl  text-gray-400 mb-4">
+                        I am ready to contribute to innovative projects and make a meaningful impact.
+                    </p>
+                    <div className=" flex flex-col sm:flex-row gap-3 mt-3">
+                        <button onClick={scrollToContact} className="p-3 w-full sm:w-fit text-white bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 rounded-full mr-4 hover:bg-opacity-90 font-bold border border-white">Hire Me</button>
+                        <button className="p-3 w-full font-bold sm:w-fit text-white bg-transparent rounded-full border border-white 
                 text-transparent bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-clip-text hover:text-slate-400 hover:border-slate-400
-                ">Download Resume
-                </button>
-            </div>
-            </div>
-            <div className="col-span-5 mt-4">
-                <div className="rounded-full bg-[#181818] flex items-center justify-center overflow-hidden">
-                <Image  
-                 src={"/avatar.png"}
-                 alt="avatar"
-                 width={300}
-                 height={300}
-                />
+                "
+                            onClick={handleResumeDownload}
+                        >
+                            Download Resume
+                        </button>
+                    </div>
+                </div>
+                <div className="col-span-5 mt-4">
+                    <div className="rounded-full bg-[#181818] flex items-center justify-center overflow-hidden">
+                        <Image
+                            src={"/avatar.png"}
+                            alt="avatar"
+                            width={300}
+                            height={300}
+                        />
+                    </div>
                 </div>
             </div>
-        </div>
         </section>
-     );
+    );
 }
- 
+
 export default HeaderSeaction;
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -7,61 +7,61 @@ import MenuOverlay from "./MenuOverlay";
 import Image from "next/image";
 
 const Navbar = () => {
-    const[navBarOpen,setNavbarOpen] = useState(true);
+    const [navBarOpen, setNavbarOpen] = useState(true);
     const scrollToAbout = () => {
         const aboutSection = document.getElementById('about'); // Id of the about section
         aboutSection.scrollIntoView({ behavior: 'smooth' });
-      };
-      const scrollToProjects = () => {
+    };
+    const scrollToProjects = () => {
         const projectsSection = document.getElementById('projects'); // Id of the about section
         projectsSection.scrollIntoView({ behavior: 'smooth' });
-      };
-      const scrollToContact = () => {
+    };
+    const scrollToContact = () => {
         const contactSection = document.getElementById('contact'); // Id of the about section
         contactSection.scrollIntoView({ behavior: 'smooth' });
-      };
-    return ( 
+    };
+    return (
         <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90 px-10">
             <div className="flex flex-wrap items-center justify-between mx-auto p-3">
-            <div className="text-2xl sm:text-3xl md:text-5xl text-white font-semibold">
-                <Link href={"/"}>
-                <Image src={"/logo2.png"} width={100} height={100} alt={"logo"} />
-                </Link>
-            </div>
-            <div className="mobile-menu block md:hidden">
-                {
-                    navBarOpen?<button onClick={() => setNavbarOpen(!navBarOpen)}><IoMdMenu className="h-5 w-5 text-white" /></button>:<button onClick={() => setNavbarOpen(!navBarOpen)}><IoCloseOutline className="h-5 w-5 text-white" /></button>
-                }
-                {
-                    !navBarOpen &&(
-                        <div>
-                            <MenuOverlay />
-                        </div>
-                    )
-                }
-            </div>
-            <div className="menu hidden md:block md:w-auto" id="nabbar" >
-            <ul className=" flex gap-x-4">
-                <li>
-                    <button onClick={scrollToAbout} href={"#about"} className="block px-2 pl-3 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white">
-                    About
-                    </button>
-                </li>
-                <li>
-                    <button onClick={scrollToProjects} href={"#projects"} className="block px-2 pl-3 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white">
-                    Projects
-                    </button>
-                </li>
-                <li>
-                    <button onClick={scrollToContact} href={"#contact"} className="block px-2 pl-3 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white">
-                    Contact
-                    </button>
-                </li>
-            </ul>
-            </div>
+                <div className="text-2xl sm:text-3xl md:text-5xl text-white font-semibold">
+                    <Link href={"/"}>
+                        <Image src={"/logo2.png"} width={100} height={100} alt={"logo"} />
+                    </Link>
+                </div>
+                <div className="mobile-menu block md:hidden">
+                    {
+                        navBarOpen ? <button onClick={() => setNavbarOpen(!navBarOpen)}><IoMdMenu className="h-5 w-5 text-white" /></button> : <button onClick={() => setNavbarOpen(!navBarOpen)}><IoCloseOutline className="h-5 w-5 text-white" /></button>
+                    }
+                    {
+                        !navBarOpen && (
+                            <div>
+                                <MenuOverlay />
+                            </div>
+                        )
+                    }
+                </div>
+                <div className="menu hidden md:block md:w-auto" id="nabbar" >
+                    <ul className=" flex gap-x-4">
+                        <li>
+                            <button onClick={scrollToAbout} href={"#about"} className="block px-2 pl-3 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white">
+                                About
+                            </button>
+                        </li>
+                        <li>
+                            <button onClick={scrollToProjects} href={"#projects"} className="block px-2 pl-3 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white">
+                                Projects
+                            </button>
+                        </li>
+                        <li>
+                            <button onClick={scrollToContact} href={"#contact"} className="block px-2 pl-3 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white">
+                                Contact
+                            </button>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
-     );
+    );
 }
- 
+
 export default Navbar;
